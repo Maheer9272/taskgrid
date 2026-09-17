edd2af57599c7cc26338d761d3a3e8c990446d99
Shaik Maheer <maheer9272@gmail.com>
2026-09-17 16:40:53 +0530

    Initial commit

---
+++ src/main/java/com/maheer/taskgrid/TaskgridApplication.java
@@ -0,0 +1,13 @@
+package com.maheer.taskgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskgridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maheer/taskgrid/TaskgridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maheer.taskgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskgridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
